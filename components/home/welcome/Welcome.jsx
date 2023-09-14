d4bf1fe5beab446c4842b9c75c1bc2680d7b6a56
it21004490 <it21004490@my.sliit.lk>
2023-09-14 11:51:27 +0530

    Tabs created

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text ,TouchableOpacity,TextInput,Image,FlatList} from 'react-native'
 import { useRouter } from 'expo-router'
 import { icons,SIZES } from '../../../constants'
 import styles from './welcome.style'
+import { JobTabs } from '../..'
+
+const jobTypes = ["Full-time","Part-time","Contractor"];
 
 const Welcome = () => {
-  const router = useRouter()
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full=time');
+  
+
   return (
     <View>
       <View style={styles.container}>
@@ -15,9 +21,35 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          
+          <TextInput
+            style={styles.searchInput}
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
         </View>
-
+          <TouchableOpacity style={styles.searchBtn}> 
+            <Image
+              source={icons.search}
+              resizeMode='contain'
+              style={styles.searchBtnImage}
+            />
+          </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
 
     </View>
